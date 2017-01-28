5dee8852ead771ee8899c7f1179f8511e462ba74
Mr Rogers <jon@bunnymatic.com>
2017-01-27 17:30:19 -0800

    update roman numberals

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,20 +1,29 @@
 defmodule Roman do
-  @roman_numerals ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
-  @decimal_values [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
-
+  @roman_with_decimal_value [ { "M", 1000 },
+                              { "CM", 900 },
+                              { "D", 500 },
+                              { "CD", 400 },
+                              { "C", 100 },
+                              { "XC", 90 },
+                              { "L", 50 },
+                              { "XL", 40 },
+                              { "X", 10 },
+                              { "IX", 9 },
+                              { "V", 5 },
+                              { "IV", 4 },
+                              { "I", 1 } ]
   @doc """
   Convert the number to a roman number.
   """
   @spec numerals(pos_integer) :: String.t
-  def numerals(number) do
-    numerals(number, @decimal_values, @roman_numerals, "")
+  def numerals(number), do: number |> numerals(@roman_with_decimal_value, "")
+  def numerals(_, [], acc), do: acc
+  def numerals(number, [{_, current_decimal} | rest], acc) when current_decimal > number do
+    number |> numerals(rest, acc)
   end
-  def numerals(_, [], [], acc), do: acc
-  def numerals(number, [cur | rest], [_ | roman], acc) when cur > number do
-    numerals(number, rest, roman, acc)
-  end
-  def numerals(number, [cur | rest], [cur_roman | roman], acc) do
-    v = number - cur
-    numerals(v, [cur | rest], [cur_roman | roman], acc <> cur_roman)
+  def numerals(number, [cur | rest], acc) do
+    { current_roman, current_decimal } = cur
+    number - current_decimal
+    |> numerals([cur | rest], acc <> current_roman)
   end
 end
