b5f86f83229604121e8c63c0ac54715eb4a92f64
Mr Rogers <jon@bunnymatic.com>
2017-01-28 17:09:14 -0800

    working on pascals triangle

--- elixir/pascals-triangle/pascals_triangle.exs
+++ elixir/pascals-triangle/pascals_triangle.exs
@@ -4,6 +4,13 @@ defmodule PascalsTriangle do
   with the given height
   """
   @spec rows(integer) :: [[integer]]
+  def rows(num) when num === 1, do: [[1]]
   def rows(num) do
+    previous_row = rows(num-1)
+    (2..num)
+    |> Enum.reduce(rows(index-1), fn(index, acc) ->
+      IO.puts("#{num} #{index} #{inspect(rows(index-1))}")
+      acc ++ [ rows(index-1) * (num - index) / (index + 1) ]
+    end)
   end
 end
