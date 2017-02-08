bdeac363cd1696d66d8d106b251707184b2d3ff7
Mr Rogers <jon@bunnymatic.com>
2017-02-08 08:42:19 -0800

    working on ruby binary search

--- ruby/binary-search-tree/binary_search_tree.rb
+++ ruby/binary-search-tree/binary_search_tree.rb
@@ -1,4 +1,49 @@
+require 'byebug'
+
 class Bst
+
+  attr_reader :data, :left, :right
+
+  def initialize(val)
+    @data = val
+    @left = nil
+    @right = nil
+  end
+
+  def insert(val)
+    val <= @data ? insert_left(val) : insert_right(val)
+  end
+
+  def each(&block)
+    fetch.each &block
+  end
+
+  protected
+  def fetch(result = [])
+    @left.fetch(result) if @left
+    @right.fetch(result) if @right
+    result << @data
+    result
+  end
+
+  private
+  def insert_left(val)
+    if @left
+      @left.insert(val)
+    else
+      @left = Bst.new(val) unless @left
+    end
+  end
+
+  def insert_right(val)
+    if @right
+      @right.insert(val)
+    else
+      @right = Bst.new(val) unless @right
+    end
+  end
+
+
 end
 
 module BookKeeping
